8a1a4455a35f95df325c4cb06a07a055defee3fb
ayatisrivastava <ayatisrivastava1906@gmail.com>
2020-08-19 20:48:50 +0530

    background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 
-var r = 0;
-var g = 50;
-var b=255;
+var r;
+var g;
+var b;
+var pointer;
 
 // INITIALIZE ALL YOUR VARIABLES
 // Ideally you would create 3 variables. red blue green or r, g, b
@@ -9,11 +10,29 @@ var b=255;
 
 function setup(){
 
-  createCanvas(1200,400);
+  createCanvas(1200,600);
+  r = createSprite(400,200,400,200);
+  r.shapeColour = "red";
+  g = createSprite(800,400,400,200);
+  g.shapeColour = "green";
+  b = createSprite(1200,600,400,200)
+  b.shapeColour = "blue";
+  pointer = createSprite(600,300,20,20)
+  pointer.x = World.mouseX;
+  pointer.y = World.mouseY;
 }
 
 
 function draw(){
+  if (r = pointer.x === pointer.width/2 + r.width/2) {
+    r.shapeColour = "green"
+  }
+  if (g = pointer.x === pointer.width/2 + g.width/2) {
+    g.shapeColour = "blue"
+  }
+  if (b = pointer.x === pointer.width/2 + b.width/2) {
+    b.shapeColour = "red"
+  }
 
   // change the value of Red based on the mouse movement about the X axis
   // change the value of Green based on the mouse movement about the X axis
